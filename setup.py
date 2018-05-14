896d9bf2f424a8da5ff10054e8e9b39fe4e2016a
Mike <msparapa@purdue.edu>
2018-05-13 21:55:22 -0400

    Changed license and added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dga',
+    version='0.1a',
+    packages=['optim', 'optim.problem', 'optim.algorithms', 'optim.algorithms.geneticalgorithm'],
+    url='https://github.com/msparapa/dga',
+    license='MIT',
+    author='Michael Sparapany',
+    author_email='msparapa@purdue.edu',
+    description='A discrete genetic algorithm.'
+)
