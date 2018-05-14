7dbd7bca40e1b288da969cb12423e3e6367b42ff
Mike <msparapa@purdue.edu>
2018-05-14 19:39:56 -0400

    Different test import statements

--- examples/tests/test_full_examples.py
+++ examples/tests/test_full_examples.py
@@ -1,4 +1,5 @@
-import dga
+from dga.problem import Problem
+from dga import dga
 
 tolerance = 1e-3
 
@@ -6,10 +7,10 @@ def test_example_quadratic1():
     def J(x):
         return x ** 2
 
-    prob = dga.Problem('quad')
+    prob = Problem('quad')
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
-    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) < tolerance
 
@@ -17,11 +18,11 @@ def test_example_quadratic2():
     def J(x, y):
         return x ** 2 + y ** 2
 
-    prob = dga.Problem('quad')
+    prob = Problem('quad')
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
     prob.State('y', lower_bound=-2, upper_bound=2, bits=12)
-    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) < tolerance
     assert abs(xopt['xopt'][1]) < tolerance
@@ -30,11 +31,11 @@ def test_example_quadratic3():
     def J(x, y):
         return x ** 2 + y ** 2
 
-    prob = dga.Problem('quad')
+    prob = Problem('quad')
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=4)
     prob.State('y', lower_bound=-2, upper_bound=2, bits=4)
-    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) - 0.1333 < tolerance
     assert abs(xopt['xopt'][1]) - 0.1333 < tolerance
