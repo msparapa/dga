f1ad363dd94ee2ffa99fe285ed5368df95dddaba
Mike <msparapa@purdue.edu>
2018-05-14 19:31:20 -0400

    Updated readme

---
+++ examples/tests/test_full_examples.py
@@ -0,0 +1,40 @@
+import dga
+
+tolerance = 1e-3
+
+def test_example_quadratic1():
+    def J(x):
+        return x ** 2
+
+    prob = dga.Problem('quad')
+    prob.Cost(J)
+    prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
+    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    xopt = GA(prob)
+    assert abs(xopt['xopt'][0]) < tolerance
+
+def test_example_quadratic2():
+    def J(x, y):
+        return x ** 2 + y ** 2
+
+    prob = dga.Problem('quad')
+    prob.Cost(J)
+    prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
+    prob.State('y', lower_bound=-2, upper_bound=2, bits=12)
+    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    xopt = GA(prob)
+    assert abs(xopt['xopt'][0]) < tolerance
+    assert abs(xopt['xopt'][1]) < tolerance
+
+def test_example_quadratic3():
+    def J(x, y):
+        return x ** 2 + y ** 2
+
+    prob = dga.Problem('quad')
+    prob.Cost(J)
+    prob.State('x', lower_bound=-2, upper_bound=2, bits=4)
+    prob.State('y', lower_bound=-2, upper_bound=2, bits=4)
+    GA = dga.dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    xopt = GA(prob)
+    assert abs(xopt['xopt'][0]) - 0.1333 < tolerance
+    assert abs(xopt['xopt'][1]) - 0.1333 < tolerance
