f25ca6d0d6b7253bc0040a4f1359f389d9eb4915
Michael Sparapany <msparapa@purdue.edu>
2018-05-22 20:09:06 -0500

    Tests use new option formatting

--- examples/tests/test_full_examples.py
+++ examples/tests/test_full_examples.py
@@ -10,7 +10,7 @@ def test_example_quadratic1():
     prob = Problem('quad')
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
-    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(display_flag=0)
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) < tolerance
 
@@ -22,7 +22,7 @@ def test_example_quadratic2():
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=12)
     prob.State('y', lower_bound=-2, upper_bound=2, bits=12)
-    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(display_flag=0)
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) < tolerance
     assert abs(xopt['xopt'][1]) < tolerance
@@ -35,7 +35,7 @@ def test_example_quadratic3():
     prob.Cost(J)
     prob.State('x', lower_bound=-2, upper_bound=2, bits=4)
     prob.State('y', lower_bound=-2, upper_bound=2, bits=4)
-    GA = dga(options=[0, 0.9, None, None, None, None, None, None, None, None, None, None, None, 200])
+    GA = dga(display_flag=0)
     xopt = GA(prob)
     assert abs(xopt['xopt'][0]) - 0.1333 < tolerance
     assert abs(xopt['xopt'][1]) - 0.1333 < tolerance
